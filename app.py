50492fac8be77ae242675b61d3191829c2b1de41
ankitshrotriya2202-rgb <ankitshrotriya2202@gmail.com>
2026-05-21 15:48:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from config import Config
 from models import db
 from routes import register_routes
+import os
+
+import requests
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
@@ -9,10 +13,54 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
-
 register_routes(app)
 
+
+# =========================
+# KEEP ALIVE ROUTE
+# =========================
+
+@app.route("/ping")
+def ping():
+    return "OK"
+
+
+# =========================
+# SELF PING FUNCTION
+# =========================
+
+def self_ping():
+    try:
+        requests.get(
+            "https://go-gas-bihc.onrender.com/ping"
+        )
+    except:
+        pass
+
+
+# =========================
+# START SCHEDULER
+# =========================
+
+scheduler = BackgroundScheduler()
+
+scheduler.add_job(
+    self_ping,
+    "interval",
+    minutes=10
+)
+
+scheduler.start()
+
+
+# =========================
+# RUN APP
+# =========================
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host='0.0.0.0',
+        port=port
+    )
